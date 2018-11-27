406b6ab9591553d99216fcaffa47007edcfe4952
Dax Huiberts <daxhuiberts@gmail.com>
2018-11-27 22:45:37 +0100

    Implement manual display trait for Conway struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-#[derive(Debug)]
+use std::fmt;
+
 struct Conway {
     width: usize,
     height: usize,
@@ -15,7 +16,25 @@ impl Conway {
     }
 }
 
+impl fmt::Display for Conway {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "+{}+\n", "--".repeat(self.width))?;
+
+        for row in self.cells.chunks(self.width) {
+            write!(f, "|")?;
+            for cell in row {
+                write!(f, "{}", if *cell { "()" } else { "  " })?;
+            }
+            write!(f, "|\n")?;
+        }
+
+        write!(f, "+{}+\n", "--".repeat(self.width))?;
+
+        Ok(())
+    }
+}
+
 fn main() {
     let conway = Conway::new(10, 10);
-    println!("{:?}", conway);
+    println!("{}", conway);
 }
