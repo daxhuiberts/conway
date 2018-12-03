e32abfcb70ddb26c6d2a43a06a930cfb2bbe7385
Dax Huiberts <daxhuiberts@gmail.com>
2018-12-03 21:53:13 +0100

    Implement next generation calculation

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,27 @@ impl Conway {
         for x in 0..self.width {
             for y in 0..self.height {
                 let cell = self.get(x, y);
-                next[x + y * self.width] = cell;
+                let live_neighbors = self.live_neighbor_count(x, y);
+                let new_cell = Self::compute_state(cell, live_neighbors);
+                next[x + y * self.width] = new_cell;
             }
         }
 
         self.cells = next;
+    }
+
+    fn live_neighbor_count(&self, x: usize, y: usize) -> usize {
+        let offsets = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
+
+        offsets.iter().filter(|(xoffset, yoffset)| {
+            let x = (x as isize + xoffset + self.width as isize) % self.width as isize;
+            let y = (y as isize + yoffset + self.height as isize) % self.height as isize;
+            self.get(x as usize, y as usize)
+        }).count()
+    }
+
+    fn compute_state(current_state: bool, live_neighbors: usize) -> bool {
+        live_neighbors == 3 || (live_neighbors == 2 && current_state == true)
     }
 }
 
